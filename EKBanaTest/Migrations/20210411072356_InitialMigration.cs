6dd649c63bf431628f7d81684198c225ab202d94
kiranbalampaki <kiranmgr1996@gmail.com>
2021-04-11 15:41:11 +0545

    Add project files.

---
+++ EKBanaTest/Migrations/20210411072356_InitialMigration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace EKBanaTest.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    EmployeeName = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Salary = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ EKBanaTest/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EKBanaTest.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Employee's Name is Required")]
+        [Display(Name = "Employee Name")]
+        public string EmployeeName { get; set; }
+        [Required(ErrorMessage = "Employee's Address is Required")]
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        [Required(ErrorMessage = "Employee's Salary is Required")]
+        public decimal Salary { get; set; }
+    }
+}
