a89ad84ca6e2ae3962b43d8dec92a9b18d4b8ef5
kiranbalampaki <kiranmgr1996@gmail.com>
2021-04-11 19:30:09 +0545

    DateValidationAdded

---
+++ EKBanaTest/wwwroot/js/customValidation.js
@@ -0,0 +1,11 @@
+﻿$("#DateOfBirth").change(function () {
+    debugger
+
+    var TodayDate = new Date();
+    var selectedDate = new Date(Date.parse($("#DateOfBirth").val()));
+
+    if (selectedDate > TodayDate) {
+        alert("Date Entered is greater than Current Date ");
+        $("#DateOfBirth").val("");
+    }
+});
